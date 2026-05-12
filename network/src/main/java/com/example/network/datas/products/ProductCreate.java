0752ec5c2222d864b69bb762d770bd424e0dbdb8
alexeys6969 <alexshashin.2023@gmail.com>
2026-05-12 15:46:24 +0500

    Разработанные ресурсы приложения. Шашин

--- network/src/main/java/com/example/network/datas/products/ProductCreate.java
+++ network/src/main/java/com/example/network/datas/products/ProductCreate.java
@@ -5,12 +5,18 @@ import android.net.Uri;
 
 import com.example.network.domains.apis.MyAsyncTask;
 import com.example.network.domains.callbacks.MyResponseCallback;
+import com.example.network.domains.common.Settings;
 import com.example.network.domains.models.Product;
 import com.example.network.domains.models.ProductBasket;
 
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
-import java.sql.Connection;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -38,7 +44,35 @@ public class ProductCreate extends MyAsyncTask {
             params.put("Expenditure", product.expenditure);
             params.put("Price", String.valueOf(product.price));
 
-
+            Connection.Response response = Jsoup.connect(Settings.URL + "/api/product/create")
+                    .ignoreContentType(true)
+                    .ignoreHttpErrors(true)
+                    .method(Connection.Method.POST)
+                    .header("token", token)
+                    .data(params)
+                    .data("ImageFile", tempFile.getName(), new FileInputStream(tempFile))
+                    .execute();
+            return response.statusCode() == 200 ? response.body() : "Error: " + response.body();
+        } catch (IOException e) {
+            return "Error: " + e.getMessage();
         }
+    }
+
+    public File createTempFileFromServer(InputStream inputStream) {
+        try {
+            File tempFile = File.createTempFile("upload_", ".jpg");
+            tempFile.deleteOnExit();
+            try(FileOutputStream out = new FileOutputStream(tempFile)) {
+                byte[] buffer = new byte[1024];
+                int bytesRead;
+                while ((bytesRead = inputStream.read(buffer)) != -1) {
+                    out.write(buffer, 0, bytesRead);
+                }
+            }
+            return tempFile;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return  null;
     }
 }
